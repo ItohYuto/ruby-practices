f5bdf0366bf5c1f6b3fd159dc053652419f48616
Yuto <agbt.itohyuto@gmail.com>
2023-11-07 14:39:59 +0900

    配列名を適切な名前に変更。

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -4,11 +4,11 @@
 FRAME_NUM = 10
 BOWL_NUM = 2
 
-input = ARGV[0].split(',')
-input_score = []
+inputs = ARGV[0].split(',')
+input_points = []
 
-input_score =
-  input.map do |val|
+input_points =
+  inputs.map do |val|
     if val == 'X'
       10
     else
@@ -20,20 +20,20 @@ bowl_cnt = 0
 total = 0
 
 (1..FRAME_NUM).each do |frame_num|
-  total += input_score[bowl_cnt]
+  total += input_points[bowl_cnt]
   bowl_cnt += 1
   if frame_num != FRAME_NUM
-    if input_score[bowl_cnt - 1] != 10
-      total += input_score[bowl_cnt]
+    if input_points[bowl_cnt - 1] != 10
+      total += input_points[bowl_cnt]
       bowl_cnt += 1
-      total += input_score[bowl_cnt] if input_score[bowl_cnt - 2] + input_score[bowl_cnt - 1] == 10
+      total += input_points[bowl_cnt] if input_points[bowl_cnt - 2] + input_points[bowl_cnt - 1] == 10
     else
-      total += input_score[bowl_cnt] + input_score[bowl_cnt + 1]
+      total += input_points[bowl_cnt] + input_points[bowl_cnt + 1]
     end
   else
-    total += input_score[bowl_cnt]
+    total += input_points[bowl_cnt]
     bowl_cnt += 1
-    total += input_score[bowl_cnt] if input_score[bowl_cnt - 2] == 10 || input_score[bowl_cnt - 2] + input_score[bowl_cnt - 1] == 10
+    total += input_points[bowl_cnt] if input_points[bowl_cnt - 2] == 10 || input_points[bowl_cnt - 2] + input_points[bowl_cnt - 1] == 10
   end
 end
 
