57ac5c534f5d2f0f537da761900dcbb1ee5efe99
Yuto <agbt.itohyuto@gmail.com>
2023-11-08 13:04:12 +0900

    不要な定数```BOWL_CNT```を削除 定数名を変更 - ```FRAME_NUM```→```MAX_FRAME_NUM``` - ```PIN_NUM```→```MAX_PIN_NUM```

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,9 +1,8 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-FRAME_NUM = 10
-BOWL_NUM = 2
-PIN_NUM = 10
+MAX_FRAME_NUM = 10
+MAX_PIN_NUM = 10
 
 inputs = ARGV[0].split(',')
 input_points = []
@@ -11,7 +10,7 @@ input_points = []
 input_points =
   inputs.map do |val|
     if val == 'X'
-      PIN_NUM
+      MAX_PIN_NUM
     else
       val.to_i
     end
@@ -20,21 +19,21 @@ input_points =
 bowl_cnt = 0
 total = 0
 
-(1..FRAME_NUM).each do |frame_num|
+(1..MAX_FRAME_NUM).each do |frame_num|
   total += input_points[bowl_cnt]
   bowl_cnt += 1
-  if frame_num != FRAME_NUM
-    if input_points[bowl_cnt - 1] != PIN_NUM
+  if frame_num != MAX_FRAME_NUM
+    if input_points[bowl_cnt - 1] != MAX_PIN_NUM
       total += input_points[bowl_cnt]
       bowl_cnt += 1
-      total += input_points[bowl_cnt] if input_points[bowl_cnt - 2] + input_points[bowl_cnt - 1] == PIN_NUM
+      total += input_points[bowl_cnt] if input_points[bowl_cnt - 2] + input_points[bowl_cnt - 1] == MAX_PIN_NUM
     else
       total += input_points[bowl_cnt] + input_points[bowl_cnt + 1]
     end
   else
     total += input_points[bowl_cnt]
     bowl_cnt += 1
-    total += input_points[bowl_cnt] if input_points[bowl_cnt - 2] == PIN_NUM || input_points[bowl_cnt - 2] + input_points[bowl_cnt - 1] == PIN_NUM
+    total += input_points[bowl_cnt] if input_points[bowl_cnt - 2] == MAX_PIN_NUM || input_points[bowl_cnt - 2] + input_points[bowl_cnt - 1] == MAX_PIN_NUM
   end
 end
 
