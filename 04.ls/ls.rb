9fc682cc8648d943e177bd5306d44812237b627b
Yuto <agbt.itohyuto@gmail.com>
2023-11-17 10:05:09 +0900

    メソッド名、変数名の修正。

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -1,47 +1,46 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# 表示列
-DISPLAY_COLUMN = 3
+MAX_COLUMN = 3
 
-def setup_display_array(files)
-  display_array = setup_empty_display_array(files)
+def align_files(files)
+  aligned_files = setup_empty_array(files)
   max_file_name_length = files.map(&:size).max
   index = 0
-  DISPLAY_COLUMN.times do |column|
-    display_array.size.times do |row|
-      display_array[row][column] = if column != DISPLAY_COLUMN - 1
+  MAX_COLUMN.times do |column|
+    aligned_files.size.times do |row|
+      aligned_files[row][column] = if column != MAX_COLUMN - 1
                                      files[index].ljust(max_file_name_length + 1)
                                    else
-                                     display_array[row][column] = files[index]
+                                     aligned_files[row][column] = files[index]
                                    end
-      return display_array if index == files.size - 1
+      return aligned_files if index == files.size - 1
 
       index += 1
     end
   end
 end
 
-def setup_empty_display_array(files)
-  quotient, remainder = files.size.divmod(DISPLAY_COLUMN)
+def setup_empty_array(files)
+  quotient, remainder = files.size.divmod(MAX_COLUMN)
   row = if remainder.zero?
           quotient
         else
           quotient + 1
         end
-  Array.new(row) { Array.new(DISPLAY_COLUMN, '') }
+  Array.new(row) { Array.new(MAX_COLUMN, '') }
 end
 
-def display_output(displays)
-  output_displays = Array.new(displays.size, '')
-  displays.size.times do |row|
-    DISPLAY_COLUMN.times do |column|
-      output_displays[row] = output_displays[row] + displays[row][column]
+def output_files(aligned_files)
+  row_strings = Array.new(aligned_files.size, '')
+  aligned_files.size.times do |row|
+    MAX_COLUMN.times do |column|
+      row_strings[row] = row_strings[row] + aligned_files[row][column]
     end
-    puts output_displays[row]
+    puts row_strings[row]
   end
 end
 
 files = Dir.glob('*')
-display_array = setup_display_array(files)
-display_output(display_array)
+aligned_files = align_files(files)
+output_files(aligned_files)
